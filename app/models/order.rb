cacefc17528e9dd4c3d1253c562efd0be7bd9fb7
Isaias David Castillo Asencio <davidc_18@me.com>
2016-10-09 12:11:37 -0500

    Cambios
    Cambios

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,9 +4,19 @@ class Order < ActiveRecord::Base
   before_create :set_order_status
   before_save :update_subtotal
 
+  belongs_to :users
+
   def subtotal
     order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
   end
+
+  def total
+      order_items.map { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+      #order_items.map{ |oi| (oi.quantity || 0) * (oi.unit_price || 0) }.sum
+
+    #order_items.sum(&:total_price)
+  end
+
 private
   def set_order_status
     self.order_status_id = 1 
@@ -17,3 +27,4 @@ private
     self[:subtotal] = subtotal 
   end
 end
+
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,7 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
+  belongs_to :tipo_producto
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validate :product_present
